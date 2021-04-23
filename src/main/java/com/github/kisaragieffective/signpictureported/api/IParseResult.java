86e92b47525657c50c5baca879e23df655cd40c7
KisaragiEffective <kisaragi.effective@gmail.com>
2021-04-24 04:14:59 +0900

    delete unused

--- src/main/java/com/github/kisaragieffective/signpictureported/api/IParseResult.java
+++
@@ -1,7 +0,0 @@
-package com.github.kisaragieffective.signpictureported.api;
-
-public interface IParseResult {
-    String getIdentifier();
-
-    DisplayConfigurationParseResult getDisplayConfiguration();
-}
